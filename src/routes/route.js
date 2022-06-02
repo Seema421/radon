122623ce9ecc47d4d0540df4e7635f88ad4e94c1
Seema 421 <seena_dalal4121@yahoo.com>
2022-06-02 20:36:36 +0530

    Part4complete of Assignment 1June

--- src/routes/route.js
+++ src/routes/route.js
@@ -22,11 +22,22 @@ router.get('/hello', function (req, res) {
 const _ = require ("lodash");
 let arr = ["Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov","Dec"]
 //making chunk of size 4
-console.log(_.chunk(arr,4))
+console.log(_.chunk(arr,3))
 const_ = require ('lodash');
 let x=[1,3,5,7,9,11,13,15,16]
 let newNum=_.tail(x);
 console.log(newNum)
+
+const u = require ("lodash")
+let arr1 = u.union ([1,2],[2,3],[1,2],[40,50],[2,40])
+console.log(arr1)
+
+const p = require('lodash')
+const fancy = [['Horror','The Shining'],['Drama','Titanic'],['Thriller','Shutter Island'],['Fantasy','Pans Labyrinth']]
+let obj=p.fromPairs(fancy);
+console.log(obj)
+
+
 
 res.send('My second ever api')
 
