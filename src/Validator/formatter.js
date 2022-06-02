e9a9a7701600360a1d500aa91a0c278ff8ce9d8f
Seema 421 <seena_dalal4121@yahoo.com>
2022-06-02 20:14:33 +0530

    number 4 part of assignment 1June

--- src/Validator/formatter.js
+++ src/Validator/formatter.js
@@ -2,7 +2,7 @@ const text = "    Function Up  "
 console.log(text.trim());
 const word = "Function Up is the bEST CODING CAMP."
 console.log(word.toLowerCase());
-const word2 =(" I am learning nodejs AND ENJOYING MUCH.JavaSCRIPT seema tough but practice makes a man perfect")
+const word2 =(" I am learning nodejs AND ENJOYING MUCH.JavaSCRIPT seems tough but practice makes a man perfect")
 console.log(word2.toUpperCase());
 
 module.exports.trim=text;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,37 @@
-const express = require('express');
+const express = require('express')
 const logger = require('../logger/logger')
 const helper =require('../util/helper')
-const router =express.Router();
 const validator=require('../validator/formatter')
+const lodash = require('express')
+
+const router =express.Router();
 
 router.get('/test-me', function (req, res) {
    logger.welcome;
-   logger.now;
-   logger.month;
-   logger.batch;
+   helper.now;
+   helper.month;
+   helper.batch;
    validator.lower;
    validator.upper;
    validator.trim;
-
-    
+ 
     res.send('My first ever api!')
 });
+router.get('/hello', function (req, res) {
+
+const _ = require ("lodash");
+let arr = ["Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov","Dec"]
+//making chunk of size 4
+console.log(_.chunk(arr,4))
+const_ = require ('lodash');
+let x=[1,3,5,7,9,11,13,15,16]
+let newNum=_.tail(x);
+console.log(newNum)
+
+res.send('My second ever api')
+
+});
+
 
 
 
