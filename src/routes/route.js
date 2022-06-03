d0bd80a647713ab13a37b5ec69e9d0c184303341
Seema 421 <seena_dalal4121@yahoo.com>
2022-06-03 22:58:03 +0530

    changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -19,18 +19,18 @@ router.get('/test-me', function (req, res) {
 });
 router.get('/hello', function (req, res) {
 
-const _ = require ("lodash");
-let arr = ["Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov","Dec"]
-//making chunk of size 4
-console.log(_.chunk(arr,3))
-const_ = require ('lodash');
-let x=[1,3,5,7,9,11,13,15,16]
-let newNum=_.tail(x);
-console.log(newNum)
+// const _ = require ("lodash");
+// let arr = ["Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov","Dec"]
+// //making chunk of size 4
+// console.log(_.chunk(arr,3))
+// const_ = require ('lodash');
+// let x=[1,3,5,7,9,11,13,15,16]
+// let newNum=_.tail(x);
+// console.log(newNum)
 
-const u = require ("lodash")
-let arr1 = u.union ([1,2],[2,3],[1,2],[40,50],[2,40])
-console.log(arr1)
+// const u = require ("lodash")
+// let arr1 = u.union ([1,2],[2,3],[1,2],[40,50],[2,40])
+// console.log(arr1)
 
 const p = require('lodash')
 const fancy = [['Horror','The Shining'],['Drama','Titanic'],['Thriller','Shutter Island'],['Fantasy','Pans Labyrinth']]
