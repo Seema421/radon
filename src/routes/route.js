269e2cc726bd33bfe33fa3a4f95c48c904e97f8c
Seema 421 <seena_dalal4121@yahoo.com>
2022-06-03 14:07:11 +0530

    Completed Assignment 1 & 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const myHelper = require('../util/helper')
-const underscore = require('underscore')
+const underscore = require('underscore');
+const { indexBy, flatten } = require('underscore');
+const { json } = require('body-parser');
 
 const router = express.Router();
 
@@ -13,21 +15,55 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
 
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
+router.get('/movies', function(req, res){
+    const movies = ['Titanic','Special 26','Bhagat Singh','Udta Punja','Love Story']
+    console.log('Hit movie lists are '+ movies)//this will print at console
+    res.send(movies)//this will got to user
+     
+
+})
+
+router.get('/movies/:indexNumber', function(req, res){
+    const movies = ['Titanic','Special 26','Bhagat Singh','Udta Punja','Love Story']
+    const movieIndex=req.params.indexNumber
+    let finalMovie= ''
+    if(movieIndex<movies.length){
+        finalMovie=movies[movieIndex];
+    } else {
+        finalMovie=("the movie with index no. doesnt exist")
+    }
+    res.send(finalMovie)// console.log(finalMovie)//y we did not wrote console because we want to send this to user
+    console.log('The request objects is '+ JSON.stringify(req.params))
+    console.log('Movies name is '+ (finalMovie))
+    // res.send('finalMovie')
+    
+})
+
+router.get('/films', function(req, res){
+    const movies = [{'id':1,'name':'The Shinning'},{'id':2,'name':'Incedies'},{'id':3,'name':'Rang De       Basanati'},{'id':4,'name':'Finding Nemo'}]
+    
+  const outcome = console.log(underscore.flatten([{'id':1,'name':'The Shinning'},{'id':2,'name':'Incedies'},{'id':3,'name':'Rang De   Basanati'},{'id':4,'name':'Finding Nemo'}]))
+   
+    res.send(movies)
+   
+    
+})
+
+router.get('/films/:filmid', function(req, res){
+    const movies = [{'id':1,'name':'The Shinning'},{'id':2,'name':'Incedies'},{'id':3,'name':'Rang De       Basanati'},{'id':4,'name':'Finding Nemo'}]
+    const movieid=req.params.filmid
+    let resultMovie=''
+    if (movieid<movies.length){
+        resultMovie=movies[movieid]
+    } else {
+        resultMovie=('This moview does not exist')
+    }
+  
+    res.send(resultMovie)
+    console.log("User is requesting for "+ JSON.stringify(resultMovie))
+   
+    
 })
 
 router.get('/candidates/:canidatesName', function(req, res){
