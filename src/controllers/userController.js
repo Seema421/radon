551a691a477288b4c89d8894cd03932dba548f3d
Seema 421 <seena_dalal4121@yahoo.com>
2022-06-06 10:22:09 +0530

    Assignment mango

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,5 +11,35 @@ const getUsersData= async function (req, res) {
     res.send({msg: allUsers})
 }
 
+const createUser1= async function (req, res) {
+    let detail = req.body
+    let savedData = await UserModel.create(detail) //use this userModel to interect with mongoDB to save our data.We are asking UserModel go ahead and create data in database
+    res.send({ data: detail })
+}
+
+const allData = async function (req, res) {
+    let detail = req.body
+    let allDetail = await UserModel.find() //use this userModel to interect with mongoDB to save our data.We are asking UserModel go ahead and create data in database
+    res.send({ data: allDetail })
+}
+const allBookData=async function(req,res){
+    let bookDetail=req.body
+    let savedData=await UserModel.create(bookDetail)
+    res.send({msg:savedData})
+   
+} 
+const getBookData=async function(req,res){
+    let bookDetail=req.body
+    let savedData=await UserModel.find()
+    res.send({data: bookDetail})
+    // let authorName=req.body.authorName;
+    console.log("Book list are "+ JSON.stringify(bookDetail))
+    console.log("Book detail is "+ JSON.stringify(bookDetail))
+}
+
+module.exports.getBookData=getBookData
+module.exports.allBookData=allBookData
+module.exports.createUser1=createUser1
+module.exports.allData=allData
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+// const UserModel= require("../models/userModel.js")
+// const UserModel=this.report('../models/userModel.js')
+const UserController = require("../controllers/userController");
+const UserModel = require('../models/userModel');//ist import to use userModel
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
+
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
+
+router.get("/test1-api", function (req, res) {
+    res.send("My api to create and save data")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/test1-post", UserController.createUser1)
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/allData", UserController.allData )
+
+router.post("/createDet8", UserController.allBookData);
+
+router.get("/getBookDetail", UserController.getBookData);
 
 module.exports = router;
