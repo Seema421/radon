e77b47a01a5f280979982a3b4e69f69fc122d49f
Seema 421 <seena_dalal4121@yahoo.com>
2022-06-20 17:54:56 +0530

    modification

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -12,7 +12,7 @@ const userSchema=new mongoose.Schema({
         enum:["Male","Female","others"]
     },
     isFreeAppUser: {type:Boolean,
-        default:true},
+        default:false},
   
     
 }, { timestamps: true });
